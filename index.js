9c739e32a8d932a946c01bd5ab8a33fabe009c3c
vargasmichael <119553981+vargasmichael@users.noreply.github.com>
2023-01-30 20:18:34 -0700

    saving the conditionals lab

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feetLimit){
+ if (feetLimit <= 400){ 
+ return  "This one is on me!" 
+ } else if (feetLimit >=400 && feetLimit <=2000)
+{ return "That will be twenty bucks."}
+ else if (feetLimit >2000, feetLimit <2500)
+ { return "I will gladly take your thirty bucks." }
+  else if (feetLimit >2500)
+  {return "No can do."}
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(destination){
+  return destination === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(response){
+  switch(response){
+    case 'generous':
+      return "Thank you so much."
+    case 'not as generous':
+      return "Thank you."
+    default:
+      return 'Bye.'
+}
 }
